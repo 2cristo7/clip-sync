de8c26f6cb3dd266d5eb3ceaf826de53d2d7b8a2
2cristo7 <diego.cristo27@gmail.com>
2026-04-14 20:05:06 +0200

    feat[mac-clipboard]: add NSPasteboard watcher with changeCount polling

---
+++ mac/ClipSync/Clipboard/ClipPayload.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+enum ClipKind: String, Codable, Sendable {
+    case text
+    case image
+}
+
+struct ClipPayload: Codable, Sendable, Equatable {
+    let type: ClipKind
+    let mime: String
+    let dataBase64: String
+    let ts: Int64
+    let nonce: String
+
+    init(type: ClipKind, mime: String, dataBase64: String, ts: Int64, nonce: String) {
+        self.type = type
+        self.mime = mime
+        self.dataBase64 = dataBase64
+        self.ts = ts
+        self.nonce = nonce
+    }
+
+    static func currentTimestampMillis() -> Int64 {
+        Int64(Date().timeIntervalSince1970 * 1000)
+    }
+
+    static func newNonce() -> String {
+        UUID().uuidString
+    }
+
+    static func text(_ value: String, ts: Int64 = ClipPayload.currentTimestampMillis()) -> ClipPayload {
+        let data = Data(value.utf8)
+        return ClipPayload(
+            type: .text,
+            mime: "text/plain",
+            dataBase64: data.base64EncodedString(),
+            ts: ts,
+            nonce: newNonce()
+        )
+    }
+
+    static func image(_ data: Data, mime: String = "image/png", ts: Int64 = ClipPayload.currentTimestampMillis()) -> ClipPayload {
+        ClipPayload(
+            type: .image,
+            mime: mime,
+            dataBase64: data.base64EncodedString(),
+            ts: ts,
+            nonce: newNonce()
+        )
+    }
+
+    var rawData: Data? {
+        Data(base64Encoded: dataBase64)
+    }
+}
